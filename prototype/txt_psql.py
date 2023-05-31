5eba774b976ba614abf2b587bd61128189d64aa3
Moos Castelijn <mooscastelijn@gmail.com>
2023-05-31 10:58:14 +0200

    initial commit

---
+++ prototype/txt_psql.py
@@ -0,0 +1,6 @@
+import psycopg2
+
+conn = psycopg2.connect(host="localhost", 
+                        database = "suppliers",
+                        user="postgres",
+                        password="postgres")
