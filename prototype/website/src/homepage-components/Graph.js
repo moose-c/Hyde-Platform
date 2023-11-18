11af20c0e6050dec96a314d005aabe08d259da56
Moos Castelijn <mooscastelijn@gmail.com>
2023-11-18 14:50:19 +0100

    Started Graph

--- prototype/website/src/homepage-components/Graph.js
+++ prototype/website/src/homepage-components/Graph.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import {useState, React} from 'react'
 
 // eslint-disable-next-line
 import Chart from 'chart.js/auto';   /* Required to mitigate some errors */
 import { Line } from 'react-chartjs-2'   /* https://github.com/reactchartjs/react-chartjs-2 */
 
 export default function Graph() {
+    const [data, setData] = useState()
+    const measurementPoints = yearNbLst.slice(startIndex, endIndex + 1)
     const options = {
         scales: {
             y: {
@@ -31,21 +33,20 @@ export default function Graph() {
         }
     }
 
-    const fetchPromise = fetch(`http://${window.apiUrl}:8000/popc/10000/bce_10000/ce_2017`).then((response) => response.json())
+    fetch(`http://${window.apiUrl}:8000/popc/10000/bce_10000/ce_2017`).then((response) => response.json())
         .then((r_json) => {
             console.log(r_json[0])
-            /* r_json[0].forEach((value, index) => {
-                allData[country.values_.ISO_A3][indicator][0].data.push({
+            r_json[0].forEach((value, index) => {
+                data.push({
                     x: measurementPoints[index],
                     y: value
                 })
             })
-            allData[country.values_.ISO_A3].all.push(allData[country.values_.ISO_A3][indicator][0])
-            allData[country.values_.ISO_A3][`${indicator}_json`] = r_json */
+            setData(data)
         })
     return (
         <div style={{ height: 300 }}>
-            {/* <Line ref={chartRef} data={data} options={{ ...options, maintainAspectRatio: false }} /> */}
+            {<Line ref={chartRef} data={data} options={{ ...options, maintainAspectRatio: false }} />}
         </div>
     )
 }
