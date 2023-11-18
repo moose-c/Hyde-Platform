8b83d508515ee92bce0e457732da6a0502362687
kanarian <arian.joyandeh@hotmail.com>
2023-11-18 13:06:02 +0100

    implement timeline

--- prototype/website/src/HomePage.js
+++ prototype/website/src/HomePage.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Timeline from "./homepage-components/Timeline";
 
 const HomePage = () => {
   return (
-    <div style={{display: "grid"}}>
+    <div style={{ display: "grid" }}>
       Home
       <Link to="/map">
         <button>Klik hier om naar de map te gaan!</button>
       </Link>
+      <div style={{ width: "100%", position: "absolute", bottom: 0 }}>
+        <Timeline />
+      </div>
     </div>
   );
 };
