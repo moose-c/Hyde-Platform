0e8e8f4a2e8d07154f3830987e36f8bb92a52336
kanarian <arian.joyandeh@hotmail.com>
2023-11-18 11:38:40 +0100

    fix basic layout

--- prototype/website/src/HomePage.js
+++ prototype/website/src/HomePage.js
@@ -1,13 +1,24 @@
+import "./styles/HomePage.css";
 import React from "react";
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div style={{display: "grid"}}>
-      Home
-      <Link to="/map">
-        <button>Klik hier om naar de map te gaan!</button>
-      </Link>
+    <div className="homepage">
+      <div className="information-overlay">
+        Information Hub Information HubInformation HubInformation Hub{" "}
+      </div>
+      <div className="graphs-overlay">
+        Graphs Overlay Graphs Overlay Graphs Overlay Graphs OverlayGraphs
+        Overlay Graphs Overlay
+      </div>
+      <div className="logo">Hyde Portal</div>
+      <div className="timeline-overlay">
+        Timeline
+        <Link to="/map">
+          <button>Klik hier om naar de map te gaan!</button>
+        </Link>
+      </div>
     </div>
   );
 };
