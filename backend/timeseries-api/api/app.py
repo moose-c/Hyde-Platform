fa4bf52dabf51d654cd085dc6a3e14f9dc99c31b
Moos Castelijn <mooscastelijn@gmail.com>
2023-12-08 18:39:15 +0100

    bezig om txt api te laten werken met n querystring ipv endpoints

--- backend/timeseries-api/api/app.py
+++ backend/timeseries-api/api/app.py
@@ -1,5 +1,5 @@
 ## Setup Flask application
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_cors import CORS
 
@@ -7,7 +7,6 @@ import os
 password = os.environ['POSTGRES_PASSWORD']
 
 app = Flask(__name__)
-api = Api(app)
 CORS(app) # mitage CORS error
 
 # Connect to database
@@ -25,8 +24,13 @@ cur.execute("SELECT * FROM uopp WHERE false")
 column_names = [desc[0] for desc in cur.description]
 
 # query and return timeseries of interest
-class Timeseries(Resource):
-  def get(self, indicator, isocode, start, end):
+@app.route('/api/txt')
+def timeseries():
+    indicator = request.args.get('indicator')
+    isocode = request.args.get('isocode')
+    start = request.args.get('start')
+    end = request.args.get('end')
+    print(start)
     # select columns = years in the correct format, including the endpoint. 
     # start end year within the url are the same format as the year columns within the database, this makes the following possible
     columns = ", ".join(column_names[column_names.index(start):column_names.index(end)+1])
@@ -36,14 +40,12 @@ class Timeseries(Resource):
     ts = cur.fetchall()
     return ts
 
-# test class to see whether the api is setup correctly
-class Test(Resource):
-  def get(self):
-    return "Setup is correct!"
-  
+# test to see whether the api is setup correctly
+@app.route('/api/test')
+def test():
+  return "Setup is correct!"
 
-
-api.add_resource(Test, '/test')
-api.add_resource(Timeseries, '/<indicator>/<isocode>/<start>/<end>')
-
-# Serving this database is handles by Gunicorn.
+# Serving this api is handles by Gunicorn.
+""" if __name__ == '__main__':
+  app.run()
+ """
